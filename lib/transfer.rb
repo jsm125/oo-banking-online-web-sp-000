c41dd47c80568ccf7af6def6f242b28bc9ec2afb
Matt Im <matthew.seiim@gmail.com>
2020-05-12 20:56:47 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,17 @@
 class Transfer
-  # your code here
+  
+  attr_accessor :status, :receiver, :amount
+  attr_reader :sender
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = 50
+    @status = "pending"
+  end
+
+  def valid?
+    BankAccount.valid?
+  end
+
 end
