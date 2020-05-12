264371b22abcaa70ad73d885919d5c528d35dbc2
Matt Im <matthew.seiim@gmail.com>
2020-05-12 22:04:35 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,8 +16,8 @@ class Transfer
 
   def execute_transaction
     if valid?
-      sender.balance = sender.balance - amount
-      receiver.balance = receiver.balance + amount
+      @sender.balance = @sender.balance - amount
+      @receiver.balance = @receiver.balance + amount
       @status = "complete"
     else
       @status = "rejected"
