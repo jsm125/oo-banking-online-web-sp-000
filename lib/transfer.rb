7716cd958a18a89932941f1514a8c15b8e511e71
Matt Im <matthew.seiim@gmail.com>
2020-05-12 22:01:04 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,10 +18,11 @@ class Transfer
     if valid?
       sender.balance = sender.balance - amount
       receiver.balance = receiver.balance + amount
-      @status = 'complete'
-    else
-      puts "Transfer rejected. Please check your account balance."
       status = "complete"
+    else
+      status = "complete"
+      puts "Transfer rejected. Please check your account balance."
+      
   end
   end
 
