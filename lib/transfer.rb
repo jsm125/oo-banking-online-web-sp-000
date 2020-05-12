fcc89265e76ce1976e6c115b76a3c8c9c75b67be
Matt Im <matthew.seiim@gmail.com>
2020-05-12 22:14:55 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,8 +20,8 @@ class Transfer
       @receiver.balance += amount
       @status = "complete"
     else
-      @status = "rejected"
       return "Transaction rejected. Please check your account balance."
+      @status = "rejected"
     end
   end
 
