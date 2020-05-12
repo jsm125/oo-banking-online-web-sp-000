1f33208cf2c8ecae306773d93461dca47bcde5f7
Matt Im <matthew.seiim@gmail.com>
2020-05-12 21:12:43 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -11,11 +11,11 @@ class Transfer
   end
 
   def valid?
-    sender.valid? && receiver.valid?
+    @sender.valid? && @receiver.valid?
   end
 
-  def execute_transaction
+  # def execute_transaction
     
-  end
+  # end
 
 end
